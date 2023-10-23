e7ad739a3e777b01fb4ec39d909bb169eb70a144
Aarón Martín <aamarber@gmail.com>
2023-10-23 09:03:52 +0200

    Tailwind solution

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     'index.html'
   ],
   theme: {
-    extend: {},
+    fontFamily:{
+      'body': ['Arial', 'sans-serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'body': '#8EABC0',
+      'orangeish': '#F6BD62',
+      'pinkish': '#EB6481',
+      'dark-orangeish': '#D76A50',
+      'post': '#B7CE3F',
+      'post-text': '#788729',
+      'footer': '#F6BD62'
+    },
+    extend:{
+      width:{
+        '200': '200px'
+      }
+    }
   },
   plugins: [],
 }
